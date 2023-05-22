0273ad1bfe10ec5401bba443e643cbaf71b46961
Mih4lych <mih4illl@gmail.com>
2023-05-22 14:23:10 +0300

    - Remove mappers in ColumnMappers.

---
+++ src/main/scala/service/db/ColumnMappers.scala
@@ -0,0 +1,52 @@
+package service.db
+import domain._
+import domain.CardRole._
+import domain.Game._
+import domain.ID._
+import domain.MoveOrder._
+import slick.jdbc.PostgresProfile.api._
+
+object ColumnMappers {
+
+
+  implicit val cardIdColumnType = MappedColumnType.base[CardId, String](
+    _.id, string => CardId(string))
+  implicit val cardStateColumnType = MappedColumnType.base[CardState, String](
+    _.entryName, string => CardState.withName(string))
+  implicit val cardRoleColumnType = MappedColumnType.base[CardRole, String](
+    _.entryName, {
+      case "RedAgent" => Agent(TeamColor.Red)
+      case "BlueAgent" => Agent(TeamColor.Blue)
+      case s if s.equals(Innocent.entryName) => Innocent
+      case s if s.equals(Assassin.entryName) => Assassin
+    })
+
+  implicit val teamIdColumnType = MappedColumnType.base[TeamId, String](
+    _.id, string => TeamId(string))
+  implicit val teamColorColumnType = MappedColumnType.base[TeamColor, String](
+    _.entryName, string => TeamColor.withName(string))
+  implicit val teamScoreColumnType = MappedColumnType.base[Score, Int](
+    _.score, int => Score(int))
+
+  implicit val playerIdColumnType = MappedColumnType.base[PlayerId, String](
+    _.id, string => PlayerId(string))
+  implicit val playerRoleColumnType = MappedColumnType.base[PlayerRole, String](
+    _.entryName, string => PlayerRole.withName(string))
+
+  implicit val gameIdColumnType = MappedColumnType.base[GameId, String](
+    _.id, string => GameId(string))
+  implicit val gameStateColumnType = MappedColumnType.base[GameState, String](
+    _.entryName, string => GameState.withName(string))
+  implicit val wordsCountColumnType = MappedColumnType.base[GameWordsCount, Int](
+    _.count, int => GameWordsCount(int))
+  implicit val moveOrderColumnType = MappedColumnType.base[MoveOrder, String](
+    _.entryName, {
+      case "RedSpymasterMove" => SpymasterMove(TeamColor.Red)
+      case "BlueSpymasterMove" => SpymasterMove(TeamColor.Blue)
+      case "RedOperativesMove" => OperativesMove(TeamColor.Red)
+      case "BlueOperativesMove" => OperativesMove(TeamColor.Blue)
+    })
+
+  implicit val resultIdColumnType = MappedColumnType.base[ResultId, String](
+    _.id, string => ResultId(string))
+}
